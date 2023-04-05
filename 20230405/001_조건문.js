47d682df9dd39eb447b7e9af06364b1cc17f0306
jypark38 <jury124@naver.com>
2023-04-05 20:39:40 +0900

    20230405 조건문

---
+++ 20230405/001_조건문.js
@@ -0,0 +1,18 @@
+let score = 69;
+let money = 1000;
+
+if (score > 90){
+    console.log('참 잘했어요 \n');
+    money += 100000;
+} else if (score > 80){
+    console.log('잘했어요 \n');
+    money += 10000;
+} else if (score > 70){
+    console.log('했어요 \n');
+    money += 1000;
+} else{
+    money = 0;
+}
+
+
+console.log(0);
