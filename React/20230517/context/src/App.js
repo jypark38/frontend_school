19971664f794eaec74ce07032d2ec62fdf232b54
jypark38 <jury124@naver.com>
2023-05-18 18:59:22 +0900

    20230518 강의자료 노트

--- React/20230517/context/src/App.js
+++ React/20230517/context/src/App.js
@@ -1,8 +1,81 @@
-function App() {
+// import { createContext } from "react"
+
+// const UserInfo = createContext({ name: "gary", id: "garyIsFree" });
+
+// const App = () => {
+//   return (
+//     <HelloLicat />
+//   );
+// };
+
+// const HelloLicat = () => {
+
+//   return (
+//     <UserInfo.Consumer>
+//       {(value) => {
+//         return <div>
+//           <h2>{value.id}</h2>
+//           <strong>{value.name}</strong>
+//           <HelloLicatTwo />
+//         </div>
+//       }}
+//     </UserInfo.Consumer>
+//   );
+// };
+
+// const HelloLicatTwo = () => {
+//   return (
+//     <UserInfo.Consumer>
+//       {(value) => {
+//         return <div>
+//           <h2>Two : {value.id}</h2>
+//           <strong>Two : {value.name}</strong>
+//         </div>
+//       }}
+//     </UserInfo.Consumer>
+//   );
+// };
+
+// export default App;
+
+
+import { createContext } from "react"
+import { useRef } from 'react';
+
+const UserInfo = createContext({ name: "gary", id: "garyIsFree" });
+
+const App = () => {
+
   return (
-    <div>
-      hello world
-    </div>
+    <HelloLicat />
   );
-}
+};
+
+const HelloLicat = () => {
+  return (
+    <UserInfo.Consumer>
+      {(value) => {
+        return <div>
+          <h2>{value.id}</h2>
+          <strong>{value.name}</strong>
+          <HelloLicatTwo />
+        </div>
+      }}
+    </UserInfo.Consumer>
+  );
+};
+
+const HelloLicatTwo = () => {
+  return (
+    <UserInfo.Consumer>
+      {(value) => {
+        return <div>
+          <h2>Two : {value.id}</h2>
+          <strong>Two : {value.name}</strong>
+        </div>
+      }}
+    </UserInfo.Consumer>
+  );
+};
+
 export default App;
